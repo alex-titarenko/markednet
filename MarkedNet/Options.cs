809db8e2a3640798832ac32008137c29aa34bd51
Tom Laird-McConnell <tomlm@microsoft.com>
2016-04-06 13:37:29 -0700

    set options back to default

--- MarkedNet/Options.cs
+++ MarkedNet/Options.cs
@@ -62,7 +62,7 @@ namespace MarkedNet
             XHtml = false;
             Sanitize = false;
             Pedantic = false;
-            Mangle = false;
+            Mangle = true;
             Smartypants = false;
             Breaks = false;
             Gfm = true;
